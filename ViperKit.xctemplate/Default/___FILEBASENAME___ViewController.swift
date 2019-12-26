340ca95be3cd8c439ee119f16cda69c7f9ec4670
Suat Karakusoglu <suat.karakusoglu@d-teknoloji.com.tr>
2019-12-26 16:16:08 +0300

    Viper module templates are prepared.

---
+++ ViperKit.xctemplate/Default/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,25 @@
+
+import UIKit
+
+final class ___VARIABLE_productName:identifier___ViewController: UIViewController, View {
+
+    typealias Event = ___VARIABLE_productName:identifier___ViewEvent
+    typealias Command = ___VARIABLE_productName:identifier___PresenterCommand
+
+    var eventListener: AnyEventListener<___VARIABLE_productName:identifier___ViewEvent>?
+    
+    func handle(command: ___VARIABLE_productName:identifier___PresenterCommand) {
+
+    }
+
+    convenience init() {
+        self.init(nibName: "\(___VARIABLE_productName:identifier___ViewController.self)", bundle: nil)
+    }
+}
+
+extension ___VARIABLE_productName:identifier___ViewController: ArenaPresenter {
+    func present(viewController: UIViewController) {
+        // navigationController?.pushViewController(viewController, animated: true)
+        print("Present view controller.")
+    }
+}
