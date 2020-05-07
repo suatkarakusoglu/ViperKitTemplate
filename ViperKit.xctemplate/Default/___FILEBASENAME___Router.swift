75b2e7f0ed5232af5fc0b818ae69a930902aa68e
Suat Karakusoglu <suat.karakusoglu@d-teknoloji.com.tr>
2020-05-07 11:53:19 +0300

    ArenaPresenter~ViewController is weakened for the router.

--- ViperKit.xctemplate/Default/___FILEBASENAME___Router.swift
+++ ViperKit.xctemplate/Default/___FILEBASENAME___Router.swift
@@ -7,7 +7,7 @@ enum ___VARIABLE_productName:identifier___Route: Route {
 final class ___VARIABLE_productName:identifier___Router: Router {
     typealias ArenaRoute = ___VARIABLE_productName:identifier___Route
 
-    var arenaPresenter: ArenaPresenter
+    weak var arenaPresenter: ArenaPresenter?
 
     init(arenaPresenter: ArenaPresenter) {
         self.arenaPresenter = arenaPresenter
